bd8dd21cf89cd70433bf9f66dbfd4f3c73891ae0
nicholas-simone <nsimone2012@gmail.com>
2025-10-28 11:33:21 -0400

    Add files via upload

---
+++ Module1.vb
@@ -0,0 +1,38 @@
+﻿Option Explicit On
+Option Strict On
+Module PayrollReport
+
+    Sub Main()
+        Dim name As String
+        Dim grossString As String
+        Dim employeeNumber As String
+        Dim bonusString As String
+        Dim gross, deduct, net, bonus As Double
+        Const RATE As Double = 0.25
+        Const QUIT As String = "XXX"
+        Const REPORT_HEADING As String = "Payroll Report"
+        Const END_LINE As String = "**End of report"
+
+        System.Console.WriteLine(REPORT_HEADING)
+        name = InputBox$("Enter employee's name: ")
+        While (name <> QUIT)
+            employeeNumber = InputBox$("Enter the employee's number: ")
+            grossString = InputBox$("Enter employee's gross pay: ")
+            bonusString = InputBox$("Enter the bonus amount: ")
+            bonus = Convert.ToDouble(bonusString)
+            gross = Convert.ToDouble(grossString)
+            deduct = gross * RATE
+            net = gross + bonus - deduct
+            System.Console.WriteLine("Name: " & name)
+            System.Console.WriteLine("Employee Number: " & employeeNumber)
+            System.Console.WriteLine("Gross Pay: " & gross)
+            System.Console.WriteLine("Deductions: " & deduct)
+            System.Console.WriteLine("Net Pay: " & net)
+            System.Console.WriteLine("Bonus: " & bonus)
+            name = InputBox$("Enter employee's name: ")
+        End While
+
+        System.Console.WriteLine(END_LINE)
+    End Sub
+
+End Module
---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Ch3_VBassignment_NCS")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Ch3_VBassignment_NCS")>
+<Assembly: AssemblyCopyright("Copyright ©  2024")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("2f3d18b2-9062-4026-a5f4-5f6824a1cbd1")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
